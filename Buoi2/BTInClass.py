187b6ecb4b538c66333cfb1d649649d798cb18d2
letienhoang <ltienhoang2@gmail.com>
2026-07-16 21:26:16 +0700

    do homework buoi 2

---
+++ Buoi2/BTInClass.py
@@ -0,0 +1,22 @@
+﻿
+nguoi = input("Nhập: ")
+
+while nguoi not in ['kéo', 'búa', 'bao']:
+    print("Nhập không hợp lệ. Vui lòng nhập lại.")
+    nguoi = input("Nhập: ")
+
+import random as rd
+
+may = rd.choice(['kéo', 'búa', 'bao'])
+
+print("Máy ra: ", may)
+
+def kq(nguoi, may):
+    if nguoi == may:
+        print("Hòa")
+    elif (nguoi == 'kéo' and may == 'bao') or (nguoi == 'búa' and may == 'kéo') or (nguoi == 'bao' and may == 'búa'):
+        print("Bạn thắng")
+    else:
+        print("Bạn thua")
+
+
